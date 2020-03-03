582ed858133406e61ddb6e7fefd42ba27197e388
zhubixia <zhubixia@xgd.com>
2020-03-03 18:41:42 +0800

    修改eslint报错警告

--- src/views/merchant-terminal/components/MerchantDialog.vue
+++ src/views/merchant-terminal/components/MerchantDialog.vue
@@ -1,31 +1,29 @@
 <template>
   <el-dialog
-    custom-class="merchant-dialog"
     :title="dialogTitle"
     :visible.sync="dialogVisible"
-    width="40%"
-  >
-  <el-form class="common-form" ref="form" :model="formData" :rules="rules" label-width="100px">
-    <el-form-item prop="merchant" label="所属商户">
-      <el-select v-model="formData.merchant">
-        <el-option
-          v-for="item in merchants" :key="item.id"
-          :value="item.id" :label="item.label">
-        </el-option>
-      </el-select>
-    </el-form-item>
-    <el-form-item label="终端号" prop="terminalId">
-      <el-input v-model="formData.terminalId" type="textarea" maxlength="200"></el-input>
-    </el-form-item>
-    <el-form-item label="备注">
-      <el-input v-model="formData.remark" type="textarea" maxlength="200"></el-input>
-    </el-form-item>
-  </el-form>
-  <span slot="footer" class="dialog-footer">
-    <el-button type="primary" class="cancel">取 消</el-button>
-    <el-button type="primary" v-if="flag===0">添 加</el-button>
-    <el-button type="primary" v-if="flag===1">保 存</el-button>
-  </span>
+    width="40%">
+    <el-form class="common-form" ref="form" :model="formData" :rules="rules" label-width="100px">
+      <el-form-item prop="merchant" label="所属商户">
+        <el-select v-model="formData.merchant">
+          <el-option
+            v-for="item in merchants" :key="item.id"
+            :value="item.id" :label="item.label">
+          </el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="终端号" prop="terminalId">
+        <el-input v-model="formData.terminalId" type="textarea" maxlength="200"></el-input>
+      </el-form-item>
+      <el-form-item label="备注">
+        <el-input v-model="formData.remark" type="textarea" maxlength="200"></el-input>
+      </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button type="primary" class="cancel" @click="dialogVisible=false">取 消</el-button>
+      <el-button type="primary" v-if="flag===0">添 加</el-button>
+      <el-button type="primary" v-if="flag===1">保 存</el-button>
+    </span>
   </el-dialog>
 </template>
 
@@ -52,7 +50,17 @@ export default {
         terminalId: [
           { required: true, message: '请输入终端编号', trigger: 'blur' }
         ]
-      }
+      },
+      merchants: [
+        {
+          value: 213233,
+          label: 'merchant111'
+        },
+        {
+          value: 23214214,
+          label: 'merchant22222'
+        }
+      ]
     }
   },
   computed: {
